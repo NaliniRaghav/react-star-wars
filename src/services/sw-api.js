f866d8e65f1882b8f7ff48de7e2dd93671bc10b3
Nalini Raghavendra <naliniraghavendra@nalinis-new-mbp.lan>
2024-09-25 11:56:51 -0400

    install axios

--- src/services/sw-api.js
+++ src/services/sw-api.js
@@ -1,7 +1,20 @@
 
-  export async function getAllStarships() {
-    const res = await fetch('https://swapi.dev/api/starships/');
-    const data = await res.json();
-    return data;  
-  }
+  // export async function getAllStarships() {
+  //   const res = await fetch('https://swapi.dev/api/starships/');
+  //   const data = await res.json();
+  //   return data;  
+  // }
   
+
+ import axios from 'axios';
+
+// Named export for fetching all starships
+export async function getAllStarships() {
+  try {
+    const res = await axios.get('https://swapi.dev/api/starships/');
+    return res.data;  
+  } catch (error) {
+    console.error("Error fetching starships:", error);
+    throw error;
+  }
+}
